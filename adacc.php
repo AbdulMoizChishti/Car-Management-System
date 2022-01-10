e0a92a26c613f2c2a2d3c2b081a6d697e3ea5c00
AbdulMoizChishti <moaiz599@gmail.com>
2022-01-11 00:00:16 +0500

    CMS

---
+++ adacc.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>HTML Forms</h2>
+
+<form action="adacc.php" method="post">
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" ><br><br>
+  <input type="submit" name="submit" value="submit">
+</form> 
+
+
+</body>
+</html>
